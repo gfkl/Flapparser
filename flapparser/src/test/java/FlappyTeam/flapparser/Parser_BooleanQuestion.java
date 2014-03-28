389d6464b138b76be15f98083e88a54e4e561401
Guillaume Gurfinkiel <pogonax@hotmail.fr>
2014-03-28 18:57:15 +0100

    OPEN - task 6: Test Parser True/false questions https://github.com/gfkl/flapparser/issues/issue/6 Création du fichier de test boolean question. Aucun test effectués pour le moment

--- flapparser/src/test/java/FlappyTeam/flapparser/Parser_BooleanQuestion.java
+++ flapparser/src/test/java/FlappyTeam/flapparser/Parser_BooleanQuestion.java
@@ -11,21 +11,16 @@ public class Parser_BooleanQuestion {
 
 	@Before
 	public void setUp() throws Exception {
-		p = new Parser();
+		p = new Parser("");
 	}
 
 	@After
 	public void tearDown() throws Exception {
 	}
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
 	
 	public void	booleanQuestion() {
-		p.parser("{Bulgaria and Rumania joined the European Union in 2007. | type=\"()\"} + TRUE. - FALSE.");
-		
+		p.setStrToParse("{Bulgaria and Rumania joined the European Union in 2007. | type=\"()\"} + TRUE. - FALSE.");
 	}
 
 }
