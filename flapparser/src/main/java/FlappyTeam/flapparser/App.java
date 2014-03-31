d2f61513fcc9e211d5bed180e4e46c7d37992501
Houpert <yoann.houpert@yahoo.fr>
2014-03-31 19:09:56 +0200

    OPEN - task 12: Réduction d'erreurs checkstyle https://github.com/gfkl/flapparser/issues/issue/12

--- flapparser/src/main/java/FlappyTeam/flapparser/App.java
+++ flapparser/src/main/java/FlappyTeam/flapparser/App.java
@@ -1,19 +1,18 @@
+/***/
 package FlappyTeam.flapparser;
 
 /***
- * 
+ *
  * @author Guillaume
  *
  */
 public class App {
-	
-	public App() {
-		
-	}
-	
+    /**@author Houpert
+     * @param args*/
     public static void main(final String[] args) {
-        String strToParse = "{La Suisse est la suisse. |type=\"{    }\"    }blablabla";
-        Parser p = new Parser(strToParse);
+        String strParse;
+        strParse = "{La Suisse est la suisse. |type=\"{    }\"   }blablabla";
+        Parser p = new Parser(strParse);
         p.doParser();
         System.out.println(p.getQuestion());
         System.out.println("Hello World!");
--- flapparser/src/main/java/FlappyTeam/flapparser/Parser.java
+++ flapparser/src/main/java/FlappyTeam/flapparser/Parser.java
@@ -2,35 +2,36 @@ package FlappyTeam.flapparser;
 
 /**@author Houpert*/
 public class Parser {
-    
-	/***/private Question question;
+
+    /***/private static Question question;
     /***/private String questionTypeStr;
     /***/private String responseStr;
     /***/private String strToParse;
 
     /**@author Houpert
-     * @param strToParse ,la chaine de charactère a parser*/
-    public Parser(final String strToParse) {
+     * @param strToParseNew ,la chaine de charactère a parser*/
+    public Parser(final String strToParseNew) {
         this.question = new Question();
         this.questionTypeStr = null;
         this.responseStr = null;
-        this.strToParse = strToParse;
+        this.strToParse = strToParseNew;
     }
 
-    /**@author Houpert*/
-    public void setStrToParse(final String strToParse) {
-        this.strToParse = strToParse;
+    /**@author Houpert
+     * @param strToParseNew ,nouvel chain a parser*/
+    public final void setStrToParse(final String strToParseNew) {
+        this.strToParse = strToParseNew;
     }
 
     /**@author Houpert
      * @return L'objet question*/
-    public Question getQuestion() {
+    public final Question getQuestion() {
         return this.question;
     }
 
     /**@author Houpert
      * @return La chaine a parser*/
-    public String getStrToParse() {
+    public final String getStrToParse() {
         return this.strToParse;
     }
 
@@ -41,9 +42,9 @@ public class Parser {
      **/
     private int takeQuestion() {
         String resQ;
-        if (this.strToParse.indexOf("|") != -1 
+        if (this.strToParse.indexOf("|") != -1
                 && this.strToParse.indexOf("{") == 0) {
-        	resQ = this.strToParse.substring(1, this.strToParse.indexOf("|"));
+            resQ = this.strToParse.substring(1, this.strToParse.indexOf("|"));
             this.question.setQuestion(resQ);
             return (this.strToParse.indexOf("|") + 1);
         } else {
@@ -64,14 +65,18 @@ public class Parser {
             temp2 = this.strToParse.indexOf("}", temp + 1);
 
             if (temp < temp2) {
-                for (int i = temp ; i <= temp2 ; i++){
-                    if (!(Character.toString(this.strToParse.charAt(i)).matches("[\\s\\n\\t]|\\}|\"")))
+                String str;
+                for (int i = temp; i <= temp2; i++) {
+                    str = Character.toString(this.strToParse.charAt(i));
+                    if (!(str.matches("[\\s\\n\\t]|\\}|\""))) {
                         return -1;
+                    }
                 }
-                this.questionTypeStr = this.strToParse.substring(value, temp2);    
+                this.questionTypeStr = this.strToParse.substring(value, temp2);
                 return (temp2);
             } else if (this.strToParse.lastIndexOf("}") >= value) {
-                this.questionTypeStr = this.strToParse.substring(value, this.strToParse.indexOf("}"));
+                temp = this.strToParse.indexOf("}");
+                this.questionTypeStr = this.strToParse.substring(value, temp);
                 return (this.strToParse.indexOf("}") + 1);
             } else {
                 return -1;
@@ -90,11 +95,11 @@ public class Parser {
      *@return la chaine de reponse
      **/
     private int takeReponseStr(final int value) {
-    	String tmpParse;
-    	int sizeStr = this.strToParse.length();
+        String tmpParse;
+        int sizeStr = this.strToParse.length();
         if (value < this.strToParse.length()) {
             tmpParse = this.strToParse.substring(value, sizeStr);
-        	this.responseStr = tmpParse;
+            this.responseStr = tmpParse;
             return 0;
         }
         return -1;
@@ -102,57 +107,58 @@ public class Parser {
 
     /**
      *@author Houpert
-     *@return la chaine parser
      **/
-    public void    doParser() {
+    public final void doParser() {
         int value = 0;
 
-        if ((value = takeQuestion()) == -1){
+        value = takeQuestion();
+        if (value == -1) {
             return;
-        } if((value = takeQuestionType(value)) == -1){
-            return ;
-        } if((value = takeReponseStr(value)) == -1) {
-            return ;
-        } if (this.question.getQuestion() != null && this.questionTypeStr != null
+        }
+        value = takeQuestionType(value);
+        if (value == -1) {
+            return;
+        }
+        value = takeReponseStr(value);
+        if (value == -1) {
+            return;
+        }
+        if (this.question.getQuestion() != null
+                && this.questionTypeStr != null
                 && this.responseStr != null) {
             getResponseByType();
         } else {
-        	return;
+            return;
         }
     }
 
 
     /**
      *@author Houpert
-     *
      *@param typeStr ,permet de generer la bonne expression reguliere
-     *
      *@return l'expression régulière complete pour le type
      **/
     private String matchedExpr(final String typeStr) {
-        String matche;
         String str = "[\\s\\n\\t]*";
+        String matche = str + "(type" + str + "=" + str + "\"" + str;
         //\\s*(type=\"\\(\\)\")\\s*
-        if(typeStr.equals("{}")) {
-            matche = str + "(type" + str + "=" + str + "\"" + str + "\\{" + str + "\\}" + str + "\")"+ str;
-        }else if(typeStr.equals("[]")) {
-            matche = str + "(type" + str + "=" + str + "\"" + str + "\\[" + str + "\\]" + str + "\")" + str;
-        }else if(typeStr.equals("()")) {
-            matche = str + "(type" + str + "=" + str + "\"" + str + "\\(" + str + "\\)" + str + "\")" + str;
-        }else {
+        if (typeStr.equals("{}")) {
+            matche += "\\{" + str + "\\}" + str + "\")" + str;
+        } else if (typeStr.equals("[]")) {
+            matche += "\\[" + str + "\\]" + str + "\")" + str;
+        } else if (typeStr.equals("()")) {
+            matche += "\\(" + str + "\\)" + str + "\")" + str;
+        } else {
             return null;
         }
         return matche;
     }
 
-    /**
-     *@author Houpert
-     *@return la question mis à jour
-     **/
+    /**@author Houpert*/
     private void getResponseByType() {
         if (this.questionTypeStr.matches(matchedExpr("()"))) {
             /*Expression pour differencier le type de question*/
-        	String matcheBool = "(\\s*[+-]\\s*(true|false|TRUE|FALSE).\\s*)*";
+            String matcheBool = "(\\s*[+-]\\s*(true|false|TRUE|FALSE).\\s*)*";
             if (this.responseStr.matches(matcheBool)) {
                 this.question.setType(TypeQuestion.bool);
                 BooleanQuestion bq = new BooleanQuestion(this.responseStr);
@@ -162,7 +168,7 @@ public class Parser {
                 this.question.setType(TypeQuestion.simple);
                 //this.question.setListeRep(parserDamien(rep));
                 System.out.println("Parser Damien");
-            }    
+            }
         } else if (this.questionTypeStr.matches(matchedExpr("[]"))) {
             this.question.setType(TypeQuestion.multiple);
             //this.question.setListeRep(parserCedric(rep));
