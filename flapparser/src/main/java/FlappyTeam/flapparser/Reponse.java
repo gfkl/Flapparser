f8a2f3e804abfdeb12ed51d0fdc78732d802e6cb
Guillaume Gurfinkiel <pogonax@hotmail.fr>
2014-04-02 19:41:26 +0200

    11: Réduction d'erreurs findbugs
    Task-Url: https://github.com/gfkl/flapparser/issues/issue/11

--- flapparser/src/main/java/FlappyTeam/flapparser/Reponse.java
+++ flapparser/src/main/java/FlappyTeam/flapparser/Reponse.java
@@ -35,6 +35,17 @@ public class Reponse {
         this.libele = libeleNew;
     }
 
+    /**
+     * @return hashcode
+     */
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((libele == null) ? 0 : libele.hashCode());
+        result = prime * result + (value ? 1231 : 1237);
+        return result;
+    }
+
     /**@param o ,object to compare
      * @return si les object sont egaux*/
     public boolean    equals(final Object o) {
