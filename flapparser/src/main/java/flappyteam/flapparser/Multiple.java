2cc551a9ad7d0879e77253749438f745190d21b2
cedaja <cedric.chauvel@yahoo.fr>
2014-04-09 11:32:01 +0200

    12: Réduction d'erreurs checkstyle
    Task-Url: https://github.com/gfkl/flapparser/issues/issue/12

--- flapparser/src/main/java/flappyteam/flapparser/Multiple.java
+++ flapparser/src/main/java/flappyteam/flapparser/Multiple.java
@@ -2,67 +2,99 @@ package flappyteam.flapparser;
 
 import java.util.ArrayList;
 
-/** 
- * Cette classe traite les questions de type Multiple-Choice 
+/**
+ * Cette classe traite les questions de type Multiple-Choice.
+ */
+
+/**
+ * @author Cédric
+ *
  */
 public class Multiple {
 
-	/**
-    * @param reponses : les reponses de la question 
-    * @param listeReponses : liste des reponses
-    */
 
-	private String reponses;
-	private ArrayList<Reponse> listeReponses;
+    /**
+     * la chaine de reponses.
+     */
+
+    private String reponses;
+
+    /**
+     * liste des reponses correspondantes a la chaine de reponses.
+     */
+    private ArrayList<Reponse> listeReponses;
+
+    /**
+     * @param rep : chaine de reponses
+     */
 
     public Multiple(final String rep) {
-    	
-    	reponses = rep;
-    	setListeReponses(new ArrayList<Reponse>());
+
+        reponses = rep;
+        setListeReponses(new ArrayList<Reponse>());
 
     }
-    
+
+    /**
+     * methode principal qui parcourt la chaine de reponses.
+     */
+
     public final void parser() {
         int indexDebut = 1;
         int indexFin = 0;
         int i = 0;
         boolean value = false;
-        
-        for (char c : reponses.toCharArray()) {
-        	
-        	if (c == '+') {
-        		value = true;
-        		indexDebut = i+2;
-        	} else if (c == '-') {
-        		value = false;
-        		indexDebut = i+2;
-        	}
 
-        	if (c == '.') {
-        		indexFin = i;
-        		createReponse(reponses.substring(indexDebut, indexFin),value);
-        	}
-        	
-        	i++;
+        for (char c : reponses.toCharArray()) {
+
+            if (c == '+') {
+                value = true;
+                indexDebut = i + 2;
+            } else if (c == '-') {
+                value = false;
+                indexDebut = i + 2;
+            }
+
+            if (c == '.') {
+                indexFin = i;
+                createReponse(reponses.substring(indexDebut, indexFin),
+                        value);
+            }
+
+            i++;
         }
 
     }
 
-	private void createReponse(String reponse, boolean value) {
+    /**
+     * @param reponse : le libele de la reponse a creer
+     * @param value : la valeur de cette reponse
+     */
 
-		Reponse newReponse = new Reponse(value,reponse);
-		
-		listeReponses.add(newReponse);
-		
-	}
+     private void createReponse(final String reponse, final boolean value) {
 
-	public ArrayList<Reponse> getListeReponses() {
-		return listeReponses;
-	}
+        Reponse newReponse = new Reponse(value, reponse);
 
-	public void setListeReponses(ArrayList<Reponse> listeReponses) {
-		this.listeReponses = listeReponses;
-	}
+        listeReponses.add(newReponse);
+
+     }
+
+     /**
+      * @return la liste des reponses
+      */
+
+    public final ArrayList<Reponse> getListeReponses() {
+        return listeReponses;
+    }
+
+    /**
+     * @param liste : la liste des reponses
+     */
+
+    public final void setListeReponses(
+            final ArrayList<Reponse> liste) {
+        this.listeReponses = liste;
+    }
 
 }
 
---
+++ flapparser/src/test/java/FlappyTeam/flapparser/ParserMultiple.java
@@ -0,0 +1,50 @@
+package FlappyTeam.flapparser;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import flappyteam.flapparser.Multiple;
+import flappyteam.flapparser.Reponse;
+
+/**
+ * @author Cédric
+ *
+ */
+
+public class ParserMultiple {
+
+    /**
+     * Nouvel objet Multiple.
+     */
+
+    private Multiple p;
+
+
+    @Before
+    public final void setUp() throws Exception {
+        p = new Multiple("+ Clojure. - Java. + Groovy. - Scala.");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testMultiple() {
+        p.parser();
+        Reponse reponse1 = p.getListeReponses().get(0);
+        Reponse reponse2 = p.getListeReponses().get(1);
+        Reponse reponse3 = p.getListeReponses().get(2);
+        Reponse reponse4 = p.getListeReponses().get(3);
+
+        assertTrue(reponse1.getLibele().equals("Clojure"));
+        assertTrue(reponse2.getLibele().equals("Java"));
+        assertTrue(reponse3.getLibele().equals("Groovy"));
+        assertTrue(reponse4.getLibele().equals("Scala"));
+
+    }
+
+}
