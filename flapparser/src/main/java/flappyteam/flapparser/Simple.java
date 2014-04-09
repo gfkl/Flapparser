30ad17567b19ef17e19ab79c35b0cfdf811afd6d
damienparise <damienparise82@gmail.com>
2014-04-09 11:18:16 +0200

    Correction checkstyle 4

--- flapparser/src/main/java/flappyteam/flapparser/Simple.java
+++ flapparser/src/main/java/flappyteam/flapparser/Simple.java
@@ -9,74 +9,74 @@ import java.util.ArrayList;
  */
 public class Simple {
 
-	/** */
-	private String reponse;
-	/** */
-	private ArrayList<Reponse> listReponse;
+    /** */
+    private String reponse;
+    /** */
+    private ArrayList<Reponse> listReponse;
 
-	/** */
-	public Simple() {
-		reponse = null;
-		listReponse = new ArrayList<Reponse>();
-	}
+    /** */
+    public Simple() {
+        reponse = null;
+        listReponse = new ArrayList<Reponse>();
+    }
 
-	/**
-	 *
-	 * @param reponseAParser : string a parser
-	 * @return : une arraylist de reponse si le string a parser est bien forme
-	 */
-	public ArrayList<Reponse> doParse(String reponseAParser) {
-		if (reponseAParser==null) {
-			return null;
-		}
-		reponse = reponseAParser;
-		if (reponse.matches("([\\s]*[\\+-][\\s\\w]+\\.)+")) {
-			while (!(reponse.isEmpty())) {
-				convertChaineToReponse(reponse.substring(0, reponse.indexOf(".")));
-				reponse = reponse.substring(reponse.indexOf(".")+1);
-			}
-		return listReponse;
-		}
-		return null;
-	}
+    /**
+     *
+     * @param reponseAParser : string a parser
+     * @return : une arraylist de reponse si le string a parser est bien forme
+     */
+    public ArrayList<Reponse> doParse(String reponseAParser) {
+        if (reponseAParser==null) {
+            return null;
+        }
+        reponse = reponseAParser;
+        if (reponse.matches("([\\s]*[\\+-][\\s\\w]+\\.)+")) {
+            while (!(reponse.isEmpty())) {
+                convertChaineToReponse(reponse.substring(0, reponse.indexOf(".")));
+                reponse = reponse.substring(reponse.indexOf(".")+1);
+            }
+            return listReponse;
+        }
+        return null;
+    }
 
-	/**
-	 *
-	 * @param chaine : contient un string contenant une reponse a transformer
-	 * ajoute la reponse a notre arrayList
-	 */
-	private void convertChaineToReponse(String chaine) {
-		if (chaine.contains("+")) {
-			String simpleReponse = chaine.substring(chaine.indexOf("+") + 1);
-			simpleReponse = supprimerEspaceDebutFinLigne(simpleReponse);
-			listReponse.add(new Reponse(true,simpleReponse));
-		}
-		else {
-			String simpleReponse = chaine.substring(chaine.indexOf("-") + 1);
-			simpleReponse = supprimerEspaceDebutFinLigne(simpleReponse);
-			listReponse.add(new Reponse(false, simpleReponse));
-		}
-	}
+    /**
+     *
+     * @param chaine : contient un string contenant une reponse a transformer
+     * ajoute la reponse a notre arrayList
+     */
+    private void convertChaineToReponse(String chaine) {
+        if (chaine.contains("+")) {
+            String simpleReponse = chaine.substring(chaine.indexOf("+") + 1);
+            simpleReponse = supprimerEspaceDebutFinLigne(simpleReponse);
+            listReponse.add(new Reponse(true,simpleReponse));
+        }
+        else {
+            String simpleReponse = chaine.substring(chaine.indexOf("-") + 1);
+            simpleReponse = supprimerEspaceDebutFinLigne(simpleReponse);
+            listReponse.add(new Reponse(false, simpleReponse));
+        }
+    }
 
-	/**
-	 *
-	 * @param chaine : chaine de caractere avec espaces en trop
-	 * @return string sans caractere d'espacement avant et apres intitule 
-	 */
-	private String supprimerEspaceDebutFinLigne(String chaine) {
-		while (chaine.substring(0, 1).equals(" ")) {
-			chaine = chaine.substring(1);
-		}
-		while (chaine.substring(chaine.length() - 1).equals(" ")) {
-			chaine = chaine.substring(0, chaine.length() - 2);
-		}
-		return chaine;
-	}
+    /**
+     *
+     * @param chaine : chaine de caractere avec espaces en trop
+     * @return string sans caractere d'espacement avant et apres intitule 
+     */
+    private String supprimerEspaceDebutFinLigne(String chaine) {
+        while (chaine.substring(0, 1).equals(" ")) {
+            chaine = chaine.substring(1);
+        }
+        while (chaine.substring(chaine.length() - 1).equals(" ")) {
+            chaine = chaine.substring(0, chaine.length() - 2);
+        }
+        return chaine;
+    }
 
-	/**
-	 * @return listReponse : ArrayList contenant toutes les objets Reponse a la question
-	 */
-	public ArrayList<Reponse> getListReponse() {
-		return listReponse;
-	}
+    /**
+     * @return listReponse : ArrayList contenant toutes les objets Reponse a la question
+     */
+    public ArrayList<Reponse> getListReponse() {
+        return listReponse;
+    }
 }
